0e4e8a3e379787d3d0cbf03088bb00f81027f5d1
davait00 <davasenior00@gmail.com>
2021-08-27 19:26:31 +0600

    dep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { User } from './Components/Users';
+import { data } from './data/data';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [age, setAge] = useState(1)
+const [country, setCountry] = useState()
+
+const renderCount =()=>{
+  switch(country){
+    case 1:
+      let arr3 = data.filter((elem)=>{
+        return elem.age<18 && elem.country === 'Kyrgyz'
+      })
+      return arr3.map((elem,id)=>{
+        return <User data={elem} key={id}/>
+      })
+    case 2: 
+    let arr4 = data.filter((elem)=>{
+      return elem.age <18 && elem.country !== 'Kyrgyz'
+    })
+    return arr4.map((elem,id)=>{
+      return <User data={elem} key={id}/>
+    })
+    default: 
+    let arr = data.filter((elem)=>{
+      return elem.age<18
+    })
+    return arr.map((elem,id)=>{
+      return <User data={elem} key={id}/>
+    })
+  }
 }
 
+const renderUsers =()=>{
+  switch(age){
+    case 1:
+      return (
+        <>
+        <div>
+          <button onClick={()=>{setCountry(1)}}>Kyrgyz</button>
+          <button onClick={()=>{setCountry(2)}}>Foreigner</button>
+          <div>{renderCount()}</div>
+        </div>
+        </>
+      )
+      case 2:
+        let arr1 = data.filter((elem)=>{
+          return elem.age >18 && elem.age <25
+        })
+        return arr1.map((elem, id)=>{
+          return <User data={elem} key={id}/>
+        })
+      case 3:
+          let arr2 = data.filter((el) => {
+            return el.age > 24
+          })
+          return arr2.map((el, id) => {
+            return <User data={el} key={id} />
+        })
+      	case 4:
+				return data.map((el, id) => {
+					return <User data={el} key={id} />
+				})
+
+			default:
+				return ''
+  }
+}
+
+  
+  return(
+    <div>
+     <h1>Users: </h1>
+     <div className="btn-group">
+      <button onClick={()=>setAge(1)} className="btn">less than 18</button>
+      <button onClick={()=>setAge(2)} className="btn">from 18  till 24</button>
+      <button onClick={()=>setAge(3)} className="btn">from 24 till 50</button>
+      <button onClick={()=>setAge(4)} className="btn">all users</button>
+     </div>
+     <div className="container">{renderUsers()}</div>
+    </div>
+  )
+}
 export default App;
---
+++ src/data/data.js
@@ -0,0 +1,94 @@
+import pic1 from '../images/kgjpg.jpg'
+import pic2 from '../images/1.jpg'
+import pic3 from '../images/avs.jpg'
+import pic4 from '../images/brazil.jpg'
+import pic5 from '../images/india.jpg'
+import pic6 from '../images/usa1.jpg'
+import pic7 from '../images/keni.jpg'
+import pic8 from '../images/korean.jpg'
+import pic9 from '../images/kg.jpg'
+import pic10 from '../images/russia.jpg'
+import pic11 from '../images/usa.jpg'
+import pic12 from '../images/kg1.jpg'
+import pic13 from '../images/uk.jpg'
+
+export const data = [
+	{
+		full_name: 'Asem A',
+		age: '17',
+		country: 'Kyrgyz',
+		img: pic1,
+	},
+	{
+		full_name: 'Linda Brown',
+		age: '35',
+		country: 'USA',
+		img: pic2,
+	},
+	{
+		full_name: 'Adam Rodriges',
+		age: '18',
+		country: 'Brazil',
+		img: pic3,
+	},
+	{
+		full_name: 'Sam K',
+		age: '50',
+		country: 'Russia',
+		img: pic4,
+	},
+	{
+		full_name: 'John Smith',
+		age: '17',
+		country: 'USA',
+		img: pic6,
+	},
+	{
+		full_name: 'Radsh Kapur',
+		age: '14',
+		country: 'India',
+		img: pic5,
+	},
+	{
+		full_name: 'Kim Yerin',
+		age: '30',
+		country: 'Korean',
+		img: pic8,
+	},
+	{
+		full_name: 'Olga Gerc',
+		age: '30',
+		country: 'Ukrain',
+		img: pic13,
+	},
+	{
+		full_name: 'Samat',
+		age: '22',
+		country: 'Kyrgyz',
+		img: pic9,
+	},
+	{
+		full_name: 'Stephanie Franz',
+		age: '44',
+		country: 'Avstria',
+		img: pic10,
+	},
+	{
+		full_name: 'Jason D',
+		age: '46',
+		country: 'USA',
+		img: pic11,
+	},
+	{
+		full_name: 'Bayaman D',
+		age: '15',
+		country: 'Kyrgyz',
+		img: pic12,
+	},
+	{
+		full_name: 'Merv T',
+		age: '37',
+		country: 'Kenia',
+		img: pic7,
+	},
+]
